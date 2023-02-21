3ef850ba164d7d229c1a98d77d8fad36ecc1ed08
Axel Vega <axel.vegal123@gmail.com>
2023-02-20 18:43:19 -0600

    Definir métodos de Personaje
    Métodos Correr, Lanzar Granada y Recargar Arma

--- Personaje.py
+++ Personaje.py
@@ -6,3 +6,16 @@ class Personaje: # Clase Personaje
     altura = 1.78
 
     #Métodos del Personaje
+    def correr(self, status): # Método correr. Siempre lleva el self xd
+        if (status):
+            print("El personaje " + self.nombre + " está corriendo")
+        else:
+            print("El personaje " + self.nombre + " está quieto")
+    
+    def lanzarGranada(self):
+        print("Se ha lanzado una granada... o un cohetazo, ahre")
+    
+    def recargarArma(self, municiones):
+        cargador = 5
+        cargador = cargador + municiones
+        print("Se han recargado " + municiones + " municiones. Ahora el cargador tiene " + cargador + " municiones")
