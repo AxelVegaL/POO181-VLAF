f79790442239310776bf230b15c3a56d262ea8a2
Axel Vega <121039412@upq.edu.mx>
2023-02-24 18:25:54 -0600

    Se agregó encapsulamiento y setters

--- Personaje.py
+++ Personaje.py
@@ -3,17 +3,17 @@ class Personaje: # Clase Personaje
     #Atributos del Personaje
     #Bob el Constructor
     def __init__(self, esp, nom, alt): # El constructor siempre lleva el self
-        self.especie = esp
-        self.nombre = nom
-        self.altura = alt #Hey, en JS se ve eso xD
+        self.__especie = esp #es público sin los "__", pero no se debe acceder a él desde fuera de la clase, por eso se usa el "__"
+        self.__nombre = nom
+        self.__altura = alt #Hey, en JS se ve eso xD
 
 
     #Métodos del Personaje
     def correr(self, status): # Método correr. Siempre lleva el self xd
         if (status):
-            print("El personaje " + self.nombre + " está corriendo")
+            print("El personaje " + self.__nombre + " está corriendo")
         else:
-            print("El personaje " + self.nombre + " está quieto")
+            print("El personaje " + self.__nombre + " está quieto")
     
     def lanzarGranada(self):
         print("Se ha lanzado una granada... o un cohetazo, ahre")
@@ -22,4 +22,26 @@ class Personaje: # Clase Personaje
         cargador = 5
         cargador = cargador + municiones
         print("Se han recargado " + str(municiones) + " municiones. Ahora el cargador tiene " + str(cargador) + " municiones")
-         
+    #Ejemplo de método privado
+    def __pensar(self):
+        print("Toy pensando... ( ͡° ͜ʖ ͡°) ")
+    
+    #getters y setters'
+
+    def getEspecie(self):
+        return self.__especie #Retorna el valor del atributo especie
+    
+    def getNombre(self):
+        return self.__nombre
+    
+    def getAltura(self):
+        return self.__altura
+    
+    def setEspecie(self, esp):
+        self.__especie = esp #Asigna un valor al atributo especie
+    
+    def setNombre(self, nom):
+        self.__nombre = nom
+
+    def setAltura(self, alt):
+        self.__altura = alt
--- objetos.py
+++ objetos.py
@@ -19,22 +19,24 @@ cargaV = int(input("Balas a recargar para el villano: "))
 Heroe = Personaje(espH, nomH, altH) # Instancia un objeto de la clase Personaje (Héroe)
 Villano = Personaje(espV, nomV, altV) # Instancia un objeto de la clase Personaje (Villano)
 #No se toma la de recarga, ya que se lleva desde Persona.py como municiones
+#Heroe.setNombre("Lucas")
+#Arriba ejemplo de una variable que ya no cambiará
 
 #4. Acceder a los atributos y métodos de cada objeto
 print("\n\nATRIBUTOS Y MÉTODOS DEL PERSONAJE\n")
-print("El personaje es un " + Heroe.especie) # Accede al atributo especie del objeto Heroe
-print("Este distinguido caballero se llama " + Heroe.nombre) # Accede al atributo nombre del objeto Heroe
-print("El personaje mide " + str(Heroe.altura) + " metros\n") # Accede al atributo altura del objeto Heroe
+print("El personaje es un " + Heroe.getEspecie()) # Accede al atributo especie del objeto Heroe
+print("Este distinguido caballero se llama " + Heroe.getNombre()) # Accede al atributo nombre del objeto Heroe
+print("El personaje mide " + str(Heroe.getAltura()) + " metros\n") # Accede al atributo altura del objeto Heroe
 Heroe.correr(True) # Accede al método correr del objeto Heroe
 Heroe.lanzarGranada() # Accede al método lanzarGranada del objeto Heroe
 Heroe.recargarArma(cargaH) # Accede al método recargarArma del objeto Heroe
+#Heroe.__pensar() # Accede al método privado pensar del objeto Heroe (NO SE PUEDE HACER XD)
 
 
 print("\n\nATRIBUTOS Y MÉTODOS DEL VILLANO\n")
-print("El villano es un " + Villano.especie)
-print("El enemigo lleva por nombre '" + Villano.nombre + "'") 
-print("El malo mide " + str(Villano.altura) + " metros\n\n")
-
+print("El villano es un " + Villano.getEspecie())
+print("El enemigo lleva por nombre '" + Villano.getNombre() + "'") 
+print("El malo mide " + str(Villano.getAltura()) + " metros\n\n")
 Villano.correr(False) 
 Villano.lanzarGranada()
 Villano.recargarArma(cargaV)
