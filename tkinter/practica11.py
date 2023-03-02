677c216e7a3e56e5dd3adb03b75abc35e66282eb
Axel Vega <121039412@upq.edu.mx>
2023-03-01 18:03:28 -0600

    Funciones de messagebox, y agregar nuevos botones dinámicamente

--- tkinter/practica11.py
+++ tkinter/practica11.py
@@ -1,4 +1,25 @@
-from tkinter import Tk, Frame, Button
+from tkinter import Tk, Frame, Button, messagebox
+
+#4. Función de mensajes para el botón
+def mostrarMensaje():
+    messagebox.showinfo("Aviso","Este mensaje es para informar algo") #Se coloca el mensaje en caja, se muestra info, título Aviso y contenido
+    messagebox.showerror("Error","Eres tú, cámbiate de carrera")
+    messagebox.askokcancel("Pregunta:","¿El o ella jugó con tu corazón?")
+    print(messagebox.askyesno("Pregunta:","El o ella jugó con tu corazao?"))
+#  print(messagebox.askretrycancel("Pregunta:","El o ella jugó con tu corazao?"))
+#   print(messagebox.askokcancel("Pregunta:","El o ella jugó con tu corazao?"))
+#    print(messagebox.askyesnocancel("Pregunta:","El o ella jugó con tu corazao?"))
+
+def mensajeBoton():
+    messagebox.showinfo("Soy el señor Meezeck, mírenme", "Mi único propósito es ser un botón funcional")
+
+#5. Función para agregar botones.
+def agregarBoton():
+    botonVerde.config(text="+", bg="green", fg="white")
+    botonNuevo= Button(seccion3, text="Botón Nuevo", command=mensajeBoton)
+    #Usamos pack porque es el que usamos de sección 3 en el paso 3
+    botonNuevo.pack()
+
 
 #1. Instanciamos un Objeto Ventana
 ventana = Tk()
@@ -15,13 +36,13 @@ seccion3 = Frame(ventana, bg="#00394d")
 seccion3.pack(expand=True, fill="both")
 
 #3. Creamos los botones
-botonAzul = Button(seccion1, text="Botón Azul Cielo", fg="#00bbff", bg="white") #seccion1 es el padre, Button la clase
+botonAzul = Button(seccion1, text="Botón Azul Cielo", fg="#00bbff", bg="white", command=mostrarMensaje) #seccion1 es el padre, Button la clase
 botonAzul.place(x=60, y=60) #x e y son las coordenadas del botón, place es el método
 botonBlanco = Button(seccion2, text="Botón Blanco", fg="white", bg="gray") #seccion1 es el padre, Button la clase
 botonBlanco.grid(row=0, column=0) #row y column son las coordenadas del botón, grid es el método
 botonAzulF = Button(seccion2, text="Botón Azul Fuerte", fg="blue", bg="white") #seccion1 es el padre, Button la clase
 botonAzulF.grid(row=1, column=1) 
-botonVerde = Button(seccion3, text="Botón Verde", fg="green", bg="white") #seccion1 es el padre, Button la clase
+botonVerde = Button(seccion3, text="Botón Verde", fg="green", bg="white", command=agregarBoton) #seccion1 es el padre, Button la clase
 botonVerde.pack() #pack es el método, no necesita coordenadas porque se pone al final de la ventana
 
 
