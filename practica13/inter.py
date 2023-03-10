8718b8ca11ba4c90ecda586f6653fc305412afd9
Axel Vega <121039412@upq.edu.mx>
2023-03-10 01:15:38 -0600

    Práctica 13 Generador de Contraseñas
    Versión ya completa, habiendo aprendido mejor a conectar lógica con clase

--- practica13/inter.py
+++ practica13/inter.py
@@ -1,46 +1,43 @@
-from tkinter import Tk, ttk, Button, messagebox
+from tkinter import Tk, Button
 from tkinter import *
 from passwords import *
 
+passw = passwords() #Este va de a fuerzas para usar la clase passwords.py
+
+def setPassword():
+    passw.createPass(length.get(),mayus.get(),special.get())
+    #Los .get() son para obtener el valor de las variables que se usan en la función createPass() de la clase passwords.py
+    #este def es el que se conecta a la class passwords.py, es decir, es el QROBús de la clase. Porque falla, pero luego sí da :D
+
+
 #Ventana de TKinter
-window = Tk()
-window.configure(background='#b3e6ff')
-window.title("Práctica 13 - Generador de Contraseñas")
-window.geometry("600x400")
+window = Tk() #Aquí se crea la ventana
+window.configure(background='#b3e6ff') #Background de la ventana
+window.title("Práctica 13 - Generador de Contraseñas") #Título de la ventana
+window.geometry("600x400") #Tamaño
 
-length = IntVar()
-ttk.lenLabel = Label(window, background='#b3e6ff', text="¿Cuántos caracteres necesitas?").pack()
-ttk.lenEntry = Entry(window, textvariable=length, show=8).pack()
+length = IntVar() #Aquí se guardará la longitud de la contraseña (por defecto es 8)
+lenLabel = Label(window, background='#b3e6ff', text="¿Cuántos caracteres necesitas?").pack()
+#aquí se crea una etiqueta con una indicación para el usuario
+lenEntry = Entry(window, textvariable=length).pack()
+#Entry para introducir una longitud de contraseña, se guarda en la variable length
 
-""" mayus = window.boolVar()
-ttk.mayusLabel = Label(window, background='#b3e6ff', text="¿Necesitas mayúsculas?").pack()
-ttk.mayusEntry = Entry(window, textvariable=mayus).pack()
-
-special = window.boolVar()
-ttk.speLabel = Label(window, background='#b3e6ff', text="¿Necesitas caracteres especiales?").pack()
-ttk.speEntry = Entry (window, text) """
-
-def maySel():
-    if (mayus.get() == TRUE):
-        print('a')
-    else:
-        print('b')
-
-def speSel():
-    if (special.get() == TRUE):
-        print('a')
-    else:
-        print('b')
-
-mayus = BooleanVar()
-special = BooleanVar()
-mayusBox = Checkbutton(window, background='#b3e6ff', text='Usar Mayúsculas',variable=mayus, onvalue=TRUE, offvalue=FALSE, command=maySel)
-mayusBox.pack()
-specialBox = Checkbutton(window, background='#b3e6ff', text='Caracteres Especial',variable=special, onvalue=TRUE, offvalue=FALSE, command=speSel)
+mayus = BooleanVar() #Estas variables son para los checkbuttons
+special = BooleanVar() #Como BoolVar() devuelve un valor booleano, se puede usar en la función createPass() de la clase passwords.py
+mayusBox = Checkbutton(window, background='#b3e6ff', text='Usar Mayúsculas',variable=mayus, onvalue=True, offvalue=False)
+#onvalue y offvalue son para que el checkbutton devuelva un valor booleano según si está activado o no el checkbutton
+mayusBox.pack() #pack() es el organizador de los destos en la ventana
+specialBox = Checkbutton(window, background='#b3e6ff', text='Caracteres Especial',variable=special, onvalue=True, offvalue=False)
 specialBox.pack()
 
-genButton = ttk.Button(window, text='Generar Contraseña', command=passwords.juan).pack()
+#genButton = ttk.Button(window, text='Generar Contraseña', command=setPassword).pack()
+genButton = Button(window, text='Generar Contraseña', command=setPassword).pack()
+#Este botón mandará a llamar a la función setPassword() que a su vez llama a la función createPass() de la clase passwords.py
 
-#ttk.passGen = Entry(window, textvariable=passw).pack()
+pLabel = Label(window, background='#b3e6ff', text="Contraseña para copypastear:").pack()
+""" pEntry = Entry(window)
+pEntry.insert(0, passw.createPass)
+pEntry.pack() """
 
 window.mainloop()
+#Este se encarga de renderizar la ventana
--- practica13/passwords.py
+++ practica13/passwords.py
@@ -1,35 +1,40 @@
 import random
 import string
+from tkinter import messagebox, Entry, Label
 lettersmay = string.ascii_letters.upper()
 lettersmin = string.ascii_letters.lower()
-digits = string.digits
-special_chars = string.punctuation
+special_chars = string.digits + string.punctuation
 
 class passwords:
-    def __init__(self):
-        self.__password = ""
-        self.__len = 8
+    def __init__(self): #Bob el constructor
+        self.__password = "" #Contraseña por defecto
+        self.__len = 8 #Longitud de la contraseña por defecto
 
-    def juan():
-        print('a')
-
-    def createPass(self,leng, mayus, spe):
-        self.__len = leng
-        if mayus == True and spe == True:
-            passPool: lettersmay+lettersmin+digits+special_chars
-        elif mayus == True and spe == False:
-            passPool: lettersmay+lettersmin
-        elif mayus == False and spe == True:
-            passPool: lettersmin+digits+special_chars
+    def createPass(self,length, mayus, special):
+        if length > 0:
+            self.__len = length #Aquí se cambia la longitud de la contraseña por la que se introdujo
         else:
-            passPool: lettersmin
-        self.__password = ''.join(random.choices(passPool,k=self.__len))
-
-
+            pass #Si no se cambia o se introduce un valor negativo, se usa la lenght por defecto
+        if mayus == True and special == True:
+            passPool= lettersmay+lettersmin+special_chars
+        elif mayus == True and special == False:
+            passPool= lettersmay+lettersmin
+        elif mayus == False and special == True:
+            passPool= lettersmin+special_chars
+        else:
+            passPool= lettersmin
+        self.__password =  "".join(random.choices(passPool,k=self.__len)) #Aquí se crea la contraseña
+        messagebox.showinfo("Contraseña creada",self.__password) #Aquí se muestra la contraseña en un messagebox
+        pEntry = Entry() #Aquí se crea un entry para copypastear la contraseña
+        pEntry.insert(0, self.__password) #Aquí se inserta la contraseña en el entry
+        pEntry.pack()
+        return self.__password
+    
+        
     
     #Getters y Setters
-    def getLen(self):
+    def getLen(self): #Este es el getter de la longitud de la contraseña
         return self.__len
     
-    def setLen(self):
-        self.__len = lenght
+    """ def setLen(self):
+        self.__len = length """
