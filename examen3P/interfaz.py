925bae63ee85e87cfa88299117ca61982a164de4
Axel Vega <121039412@upq.edu.mx>
2023-04-19 18:08:54 -0600

    Examen 3P - Commit 6:10

---
+++ examen3P/interfaz.py
@@ -0,0 +1,121 @@
+from tkinter import *
+from tkinter import ttk #importamos ttk para usar NoteBook
+import tkinter as tk
+from logica import * #1. Presentamos los archivos Controlador y Vista
+
+#2. Creamos 1 objeto de la clase controladorBD
+controlador = logica() #Como en las otras prácticas, creamos un objeto de la clase
+
+def ejecutaInsert():
+    controlador.guardarExportacion(varTransporteNew.get(), varAduanaNew.get())
+
+def ejecutaSelectExpo():
+    controlador.consultarExportacion(varbus.get()) #Aquí se manda el id a la función de la clase controladorBD. Variable Buscar: varbus
+
+    for expo in exportacion:
+        cadena = "Id: " + str(expo[0])+ "\n" + " Transporte: " + expo[1]+ "\n" + " Aduana: " + expo[2]
+    
+    if (exportacion):
+        print(cadena)
+        #actualizar textEnc
+        textEnc.delete("1.0", END)
+        textEnc.insert(END, cadena)
+
+    else:
+        messagebox.showinfo("Error", "No se encontró la exportación")
+
+def allSelect():
+    #limpiar el TreeView
+    tabla.delete(*tabla.get_children())
+    exportaciones = controlador.consultarTodasExportaciones()
+    for expo in exportaciones:
+        cadena = "Id: " + str(expo[0])+ "\n" + " Transporte: " + expo[1]+ "\n" + " Aduana: " + expo[2]
+        print(cadena)
+        #colocar en el TreeView los datos de la consulta
+        tabla.insert('',0, values=(expo[0],expo[1], expo[2]))
+
+""" def ejecutaActualizacion():
+    controlador.actualizarExportacion(varID.get(), varTransporte.get(), varAduana.get()) """
+
+def ejecutaEliminar():
+    controlador.eliminarExportacion(varDeleteID.get())
+
+ventana = Tk()
+ventana.title("CRUD de exportaciones - Examen 3P")
+ventana.geometry("1024x400")
+
+panel = ttk.Notebook(ventana) #creamos el panel de pestañas
+panel.pack(fill='both', expand='yes') #lo empaquetamos
+
+pestana1 = ttk.Frame(panel) #creamos la primera pestaña
+pestana2 = ttk.Frame(panel) #creamos la segunda pestaña
+pestana3 = ttk.Frame(panel) #creamos la tercera pestaña
+pestana4 = ttk.Frame(panel) #creamos la cuarta pestaña
+
+#Pestaña 1
+panel.add(pestana1, text='Insertar') #agregamos la pestaña al panel
+panel.add(pestana2, text='Consultar') #agregamos la pestaña al panel
+panel.add(pestana3, text='Todos los registros') #agregamos la pestaña al panel
+panel.add(pestana4, text='Eliminar') #agregamos la pestaña al panel
+
+#Pestaña 1
+#Etiquetas
+lblTransporteNew = Label(pestana1, text="Transporte: ")
+lblTransporteNew.grid(column=0, row=0)
+lblAduanaNew = Label(pestana1, text="Aduana: ")
+lblAduanaNew.grid(column=0, row=1)
+
+#Cajas de texto
+varTransporteNew = StringVar()
+txtTransporteNew = Entry(pestana1, width=10, textvariable=varTransporteNew)
+txtTransporteNew.grid(column=1, row=0)
+varAduanaNew = StringVar()
+txtAduanaNew = Entry(pestana1, width=10, textvariable=varAduanaNew)
+txtAduanaNew.grid(column=1, row=1)
+
+#Botón
+btnInsertar = Button(pestana1, text="Insertar", command=ejecutaInsert)
+btnInsertar.grid(column=1, row=2)
+
+#Pestaña 2
+#Etiquetas
+lblbus = Label(pestana2, text="Buscar: ")
+lblbus.grid(column=0, row=0)
+
+#Cajas de texto
+varbus = StringVar()
+txtbus = Entry(pestana2, width=10, textvariable=varbus)
+txtbus.grid(column=1, row=0)
+
+#Botón
+btnBuscar = Button(pestana2, text="Buscar", command=ejecutaSelectExpo)
+btnBuscar.grid(column=1, row=1)
+
+#Pestaña 3
+#Botón
+btnTodos = Button(pestana3, text="Mostrar todos", command=allSelect)
+btnTodos.grid(column=0, row=0)
+
+#Tabla
+tabla = ttk.Treeview(pestana3, columns=(1,2,3), show="headings", height="5")
+tabla.grid(column=0, row=1)
+tabla.heading(1, text="Id")
+tabla.heading(2, text="Transporte")
+tabla.heading(3, text="Aduana")
+
+#Pestaña 4
+#Etiquetas
+lblDeleteID = Label(pestana4, text="Id: ")
+lblDeleteID.grid(column=0, row=0)
+
+#Cajas de texto
+varDeleteID = StringVar()
+txtDeleteID = Entry(pestana4, width=10, textvariable=varDeleteID)
+txtDeleteID.grid(column=1, row=0)
+
+#Botón
+btnEliminar = Button(pestana4, text="Eliminar", command=ejecutaEliminar)
+btnEliminar.grid(column=1, row=1)
+
+ventana.mainloop()
+
---
+++ examen3P/logica.py
@@ -0,0 +1,110 @@
+from tkinter import messagebox
+import sqlite3
+
+class logica:
+
+    def __init__(self):
+        pass
+
+    #Método para crear conexiones
+    def conexionBD(self):
+        try: #lo que intentamos hacer
+            conexion = sqlite3.connect("C:/Users/axelv/Desktop/GitPoo/POO181/examen3P/BDExportaciones.db")
+            print ("Conexión exitosa")
+            return conexion
+        except sqlite3.OperationalError:
+        #si hay un error, qué hacer
+            print ("Error al conectar a la base de datos")
+
+    #Método para guardar Exportaciones
+    def guardarExportacion(self, trans, adua): #recibe los datos de exportación
+        #1. Conectar a la base de datos
+        conx = self.conexionBD() #llamamos al método de conexión
+        #2. Validar que no hayan campos vacíos
+        if (trans == "" or adua == ""):
+            messagebox.showwarning("Campos vacíos", "Procura incluir todos los datos")
+        else:
+            #3. Preparamos Cursor, Datos, QuerySQL
+            cursor = conx.cursor() #cursor permite insertar datos a la BD con SQLite
+            datos = (trans, adua)
+            qrInsert = "INSERT INTO TBPedimentos(Transporte, Aduana) VALUES(?,?)"
+            #qrInsert mete los datos en la tabla y los values serán incógnitas
+
+            #4. Ejecutamos Insert y cerramos la conexión
+            cursor.execute(qrInsert, datos)
+            conx.commit() #commit es para guardar los cambios
+            conx.close() #cerramos la conexión
+            messagebox.showinfo("Registro exitoso", "Exportación registrada con éxito")
+
+    #Método para buscar Exportaciones
+    def consultarExportacion(self,id):
+        #1. Preparamos la conexión
+        conx = self.conexionBD()
+
+        #2. Verificar si ID no está vacío
+        if (id == ""): #También se puede validar datos numéricos con isdigit()
+            messagebox.showwarning("Campos vacíos", "Procura incluir todos los datos")
+            conx.close()
+        else:
+            try:
+                #3. Preparamos Cursor, Datos, QuerySQL
+                cursor = conx.cursor()
+                qrSelect = "SELECT * FROM TBPedimentos WHERE id = ?"
+                #4. Ejecutamos Select y cerramos la conexión
+                cursor.execute(qrSelect)
+                registro = cursor.fetchone()
+                conx.close()
+                return registro
+            except sqlite3.OperationalError:
+                messagebox.showwarning("Error", "No se encontró el registro")
+                conx.close()
+
+    #Método para consultar todas las exportaciones
+    def consultarTodasExportaciones(self):
+        #1. Preparamos la conexión
+        conx = self.conexionBD()
+        try:
+            #2. Preparamos Cursor, Datos, QuerySQL
+            cursor = conx.cursor()
+            qrSelect = "SELECT * FROM TBPedimentos"
+            #3. Ejecutamos Select y cerramos la conexión
+            cursor.execute(qrSelect)
+            registros = cursor.fetchall()
+            conx.close()
+            return allRegistros
+        except sqlite3.OperationalError:
+            messagebox.showwarning("Error", "No se encontró el registro")
+            conx.close()
+
+    #Método para eliminar Exportaciones
+    def eliminarExportacion(self, id):
+        #1. Preparamos la conexión
+        conx = self.conexionBD()
+
+        #2. Verificar si ID no está vacío
+        if (id == ""):
+            messagebox.showwarning("Campos vacíos", "Procura incluir todos los datos")
+            conx.close()
+        else:
+            validar = messagebox.askquestion("Eliminar", "¿Estás seguro de eliminar el registro?")
+            if validar:
+                try:
+                    rsExport = self.consultarExportacion(id)
+                    if (rsExport == []):
+                        messagebox.showwarning("Error", "No se encontró el registro")
+                        conx.close()
+                    else:
+                        #3. Preparamos Cursor, Datos, QuerySQL
+                        cursor = conx.cursor()
+                        qrDelete = "DELETE FROM TBPedimentos WHERE id = ?"
+                        #4. Ejecutamos Delete y cerramos la conexión
+                        cursor.execute(qrDelete)
+                        conx.commit()
+                        conx.close()
+                        messagebox.showinfo("Eliminación exitosa", "Exportación eliminada con éxito")
+                except sqlite3.OperationalError:
+                    messagebox.showwarning("Error", "No se encontró el registro")
+                    conx.close()
+            else:
+                conx.close()
+                messagebox.showinfo("Eliminación cancelada", "No se eliminó el registro")
